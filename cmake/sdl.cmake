136611661431a557b4b2bc96fdd8b5dc2d9295f8
Johan Rönnkvist <johan.ronnkvist@gmail.com>
2018-10-26 22:08:20 +0200

    added configration for sdl libraries

---
+++ cmake/sdl.cmake
@@ -0,0 +1,18 @@
+if (APPLE)
+    set(SDL2_PATH       "${PROJECT_SOURCE_DIR}/third-party/sdl/osx")
+    set(SDL2_IMAGE_PATH "${PROJECT_SOURCE_DIR}/third-party/sdl/osx")
+    set(SDL2_TTF_PATH   "${PROJECT_SOURCE_DIR}/third-party/sdl/osx")
+    set(SDL2_MIXER_PATH "${PROJECT_SOURCE_DIR}/third-party/sdl/osx")
+else()
+    set(SDL2_PATH       "${PROJECT_SOURCE_DIR}/third-party/sdl/win/SDL2-2.0.8")
+    set(SDL2_IMAGE_PATH "${PROJECT_SOURCE_DIR}/third-party/sdl/win/SDL2_image-2.0.3")
+    set(SDL2_TTF_PATH   "${PROJECT_SOURCE_DIR}/third-party/sdl/win/SDL2_ttf-2.0.14")
+    set(SDL2_MIXER_PATH "${PROJECT_SOURCE_DIR}/third-party/sdl/win/SDL2_mixer-2.0.2")
+endif()
+
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${PROJECT_SOURCE_DIR}/cmake")
+
+find_package(SDL2 REQUIRED)
+find_package(SDL2_image REQUIRED)
+find_package(SDL2_ttf REQUIRED)
+find_package(SDL2_mixer REQUIRED)
